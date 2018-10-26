67462cc4a98c904d012a25dacae51a917b804c46
Charvi bansal <32154571+charvi1234@users.noreply.github.com>
2018-10-26 13:01:59 +0530

    Add files via upload

---
+++ check_cyclic_path_of_robot.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void main()
+{
+    char path[10000];
+    scanf("%s",path);
+    int north=0,east=1,south=2,west=3;
+    int dir=north;
+    int x=0,y=0,i;
+    for(i=0;path[i]!='\0';i++)
+    {
+        char move=path[i];
+        if(move=='r')
+        {
+            dir=(dir+1)%4;
+        }
+        else if(move=='l')
+        {
+            dir=(dir+3)%4;
+        }
+        else if(move=='g')
+        {
+            if(dir==north)
+                y++;
+            else if(dir==south)
+                y--;
+            else if(dir==east)
+                x++;
+            else if(dir==west)
+                x--;
+        }
+    }
+    if(x==0&&y==0)
+        printf("yes path is cyclic");
+    else
+        printf("no path is not cyclic");
+}
